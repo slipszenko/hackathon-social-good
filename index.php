6b155f1674df23cb4b724351bb87648aae62ed67
Edward Slipszenko <edward.slipszenko@myport.ac.uk>
2014-06-14 12:25:38 +0200

    Edd can't spell.

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
-require('vender/autoload.php');
+require('vendor/autoload.php');
 
 require('config.php');
 
 FacebookSession::setDefaultApplication($config['fb']['id'], $config['fb']['secret']);
+
+
+$helper = new FacebookRedirectLoginHelper('your redirect URL here');
+$loginUrl = $helper->getLoginUrl();
+
+
 ?>
+<a href="<?=$loginUrl?>">Facebook Login</a>
 
 Hello, world
